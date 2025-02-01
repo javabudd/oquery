78bbbfaa915c70fafa2da0d2cb541021018d1a1a
andyo <javabudd@gmail.com>
2025-02-01 15:44:34 -0700

    remove div

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,9 +6,7 @@ function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<div>
-					<QueryLlmComponent/>
-				</div>
+				<QueryLlmComponent/>
 			</header>
 		</div>
 	);
