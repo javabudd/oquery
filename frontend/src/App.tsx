36112ec547cc68a4115598f7e63c105f50fefc82
javabudd <javabudd@gmail.com>
2025-02-02 17:03:29 -0700

    Add cloudwatch rum (#5)
    Adds RUM tracking for sessions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,31 @@
+import {AwsRum, AwsRumConfig} from 'aws-rum-web';
 import React from 'react';
 import QueryLlmComponent from "./QueryLlmComponent";
 import './App.css';
+
+try {
+	const config: AwsRumConfig = {
+		sessionSampleRate: 1,
+		identityPoolId: process.env.AWS_RUM_IDENTITY_POOL_ID,
+		endpoint: "https://dataplane.rum.us-west-2.amazonaws.com",
+		telemetries: ["performance", "errors", "http"],
+		allowCookies: true,
+		enableXRay: false
+	};
+
+	const APPLICATION_ID: string = process.env.AWS_RUM_APPLICATION_ID ?? '0';
+	const APPLICATION_VERSION: string = '1.0.0';
+	const APPLICATION_REGION: string = process.env.AWS_RUM_REGION ?? '0';
+
+	new AwsRum(
+		APPLICATION_ID,
+		APPLICATION_VERSION,
+		APPLICATION_REGION,
+		config
+	);
+} catch (error) {
+	// Ignore errors thrown during CloudWatch RUM web client initialization
+}
 
 function App() {
 	return (
