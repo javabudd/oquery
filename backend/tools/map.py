c5cdcc04aebe7dc934e105379ae209cc2eedbed6
andyo <javabudd@gmail.com>
2025-02-13 20:09:20 -0700

    Add mapping

--- backend/tools/map.py
+++ backend/tools/map.py
@@ -1,6 +1,7 @@
 from tools.sports.nba.get_schedule import lookup_nba_schedule, lookup_nba_schedule_def
 
 AVAILABLE_FUNCTIONS = {
+    'get_nba_schedule': (lookup_nba_schedule_def, lookup_nba_schedule),
     'lookup_nba_schedule': (lookup_nba_schedule_def, lookup_nba_schedule),
     'nbaSchedule': (lookup_nba_schedule_def, lookup_nba_schedule),
 }
